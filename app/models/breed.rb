85ef740e99ae7c816b30db1df6ac70d1ce7593bd
abhatti2 <abhatti2@rrc.ca>
2024-10-28 01:42:34 -0500

    add validations for breed model

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,10 @@
 class Breed < ApplicationRecord
-  has_many :comments
+  # Association with comments
+  has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :name, presence: true, uniqueness: true
+
+  # validate sub_breed
+  validates :sub_breed, allow_blank: true, length: { maximum: 100 }
 end
