2b8f040e6e1a3e26dbd8b8e544834a4686310cf0
abhatti2 <abhatti2@rrc.ca>
2024-10-28 01:44:27 -0500

    add validations to Comment Model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
+  # Associations
   belongs_to :user
   belongs_to :breed
+
+  # Validations
+  validates :content, presence: true, length: { minimum: 5 }  # Content must be present and at least 5 characters long
+
+  # validate associations
+  validates :user_id, presence: true
+  validates :breed_id, presence: true
 end
