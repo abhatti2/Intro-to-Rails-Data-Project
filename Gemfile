5049c37b415df4e4b882d5b8dccd7a1fe83c0a90
abhatti2 <abhatti2@rrc.ca>
2024-10-28 02:11:21 -0500

    add pagination for breeds_controller.rb

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem "rails-erd"
 
 gem "httparty"
 
+gem "kaminari"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
 
--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,14 +1,20 @@
 class BreedsController < ApplicationController
   def index
+    # If there's a search term, filter breeds by name or sub_breed
     if params[:search].present?
       @breeds = Breed.left_joins(:comments)
                      .where("breeds.name LIKE ? OR breeds.sub_breed LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
                      .group(:id)
                      .select("breeds.*, COUNT(comments.id) AS comments_count")
+                     .page(params[:page])
+                     .per(10)  # Display 10 breeds per page
     else
+      # If no search term, display all breeds with comment counts
       @breeds = Breed.left_joins(:comments)
                      .group(:id)
                      .select("breeds.*, COUNT(comments.id) AS comments_count")
+                     .page(params[:page])
+                     .per(10)  # Display 10 breeds per page
     end
   end
 end
