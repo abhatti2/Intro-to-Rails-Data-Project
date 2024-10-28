3640e6602414a7d01aae94d82184b73d39313693
abhatti2 <abhatti2@rrc.ca>
2024-10-28 02:13:34 -0500

    add pagination to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def index
     if params[:search].present? && !params[:search].empty?
-      # Search by name or email, ensuring no null search
+      # Search by name or email
       @users = User.where("name LIKE ? OR email LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+                   .page(params[:page])  # Add pagination here
+                   .per(10)  # Display 10 users per page
     else
-      @users = User.all
+      # Display all users with pagination
+      @users = User.page(params[:page])  # Add pagination here
+                   .per(10)  # Display 10 users per page
     end
   end
 end
