ffd17fa1c00bbf7d5d949eed52cd35e035ba12b0
abhatti2 <abhatti2@rrc.ca>
2024-10-28 01:46:58 -0500

    add validations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  has_many :comments
+  # Association with comments
+  has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # Ensures that email uniqueness is not case-sensitive (i.e., user@example.com and USER@EXAMPLE.COM would be considered the same).
+  validates :email, uniqueness: { case_sensitive: false }
 end
